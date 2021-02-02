de3fbd409497022d7cc3eb5a4ef23ff5a09e7e52
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2021-02-02 15:28:11 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,6 +5,10 @@ echo "function stale() {" >> .bashrc
 echo "  /root/./v2raymenu.sh" >> .bashrc
 echo "}" >> .bashrc
 wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/try.sh -O /root/v2ray.sh && cd /root && chmod +x v2ray.sh
+wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/cert.sh -O /root/cert.sh && cd /root && chmod +x cert.sh
+wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/domain.sh -O /root/domain.sh && cd /root && chmod +x domain.sh
+wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/uuid.sh -O /root/uuid.sh && cd /root && chmod +x uuid.sh
+wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/key.sh -O /root/key.sh && cd /root && chmod +x key.sh
 clear
 echo "Enter the command stale to open menu"
 echo "If it doesn't work, enter the command"
