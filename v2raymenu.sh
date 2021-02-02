e4a99107be669c0468f3a4f1e79a04d56465e0d9
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2021-02-02 15:02:02 +0800

    Update v2raymenu.sh

--- v2raymenu.sh
+++ v2raymenu.sh
@@ -1,1 +1,17 @@
-#/bin/bash
+#!/bin/bash
+
+clear
+  echo "1) Install V2ray Vmess Protocol + Websocket + TLS"
+  echo "2) Generate UUID"
+  echo "3) V2ray Server Details"
+  echo "0) Exit"
+  echo ""
+  echo ""
+  echo -e "Enter your response \c"
+  read -r answer
+  case "$answer" in
+    1) clear && cd /root && ./cert.sh
+    2) curl -skL -w "\n" https://www.uuidgenerator.net/api/version4;;
+    3) nano /root/v2rayserver;;
+    0) clear && exit;;
+  esac
