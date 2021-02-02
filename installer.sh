47fe3189f87ba48390876a7b8779b3f43b9fb3df
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2021-02-02 15:30:07 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,6 +9,7 @@ wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/cert.sh -O /root/ce
 wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/domain.sh -O /root/domain.sh && cd /root && chmod +x domain.sh
 wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/uuid.sh -O /root/uuid.sh && cd /root && chmod +x uuid.sh
 wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/key.sh -O /root/key.sh && cd /root && chmod +x key.sh
+wget -q https://raw.githubusercontent.com/Stale-1/V2ray/main/v2rayserver
 clear
 echo "Enter the command 'stale' to open menu"
 echo "If it doesn't work, enter the command"
